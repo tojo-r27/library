e973143390bea41cf1769d2b2f6c00c1d40cc8ad
Tojo R. <tojorodialson@gmail.com>
2025-08-13 12:32:29 +0300

    refactor: remove unused OAuthenticatable interface from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\Contracts\OAuthenticatable;
 use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable implements OAuthenticatable
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
